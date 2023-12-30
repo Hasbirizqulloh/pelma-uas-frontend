03ae569a414c894c0bbed2fcacf90a9a485b7f93
Hasbi Rizqulloh <hasbirzq08@gmail.com>
2023-12-31 06:42:34 +0700

    revisi

--- src/component/HomeAdmin.jsx
+++ src/component/HomeAdmin.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill } from 'react-icons/bs';
-// import ListLaporanDb from './ListLaporanDb';
 import ListUserDb from './ListUserDb';
 import ListLaporanDb from './ListLaporanDb';
+import { getUsers, getReport } from '../features/authSlices';
 
 const HomeAdmin = () => {
+  const [users, setUsers] = useState([]);
+  const [reports, setReports] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const token = localStorage.getItem('Authorization');
+
+  useEffect(() => {
+    getUsers((data) => {
+      setUsers(data);
+    }, token);
+  }, [token]);
+
+  useEffect(() => {
+    getReport((data) => {
+      setReports(data);
+    }, token);
+  }, []);
+
   return (
     <main className="main-container">
       <div className="main-title">
@@ -17,28 +34,28 @@ const HomeAdmin = () => {
             <h3>USER</h3>
             <BsFillArchiveFill className="card_icon" />
           </div>
-          <h1>300</h1>
+          <h1>{users.length}</h1>
         </div>
         <div className="cards">
           <div className="card-inner">
             <h3>LAPORAN</h3>
             <BsFillGrid3X3GapFill className="card_icon" />
           </div>
-          <h1>12</h1>
+          <h1>{reports.length}</h1>
         </div>
         <div className="cards">
           <div className="card-inner">
             <h3>KOMUNITAS</h3>
             <BsPeopleFill className="card_icon" />
           </div>
-          <h1>33</h1>
+          <h1>0</h1>
         </div>
         <div className="cards">
           <div className="card-inner">
             <h3>TINDAKAN</h3>
             <BsFillBellFill className="card_icon" />
           </div>
-          <h1>42</h1>
+          <h1>0</h1>
         </div>
       </div>
       <ListLaporanDb />
